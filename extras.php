5462011da38d1f79ab8e9b938926759af1435fee
John McGettrick <jmcgettrick@iparadigms.com>
2014-11-11 10:58:04 +0100

    Enrol instructors before opening DV.

--- extras.php
+++ extras.php
@@ -63,6 +63,10 @@ switch ($cmd) {
     case "grademark":
         $submissionid = required_param('submissionid', PARAM_INT);
         $user = new turnitintooltwo_user($USER->id, $userrole);
+        if ($userrole == 'Instructor') {
+            $coursedata = turnitintooltwo_assignment::get_course_data($cm->course, 'PP');
+            $user->join_user_to_class($coursedata->turnitin_cid);
+        }
         echo html_writer::tag("div", $turnitintooltwoview->output_dv_launch_form($cmd, $submissionid, $user->tii_user_id, $userrole, ''),
                                                                                 array("class" => "launch_form"));
         echo html_writer::script("<!--

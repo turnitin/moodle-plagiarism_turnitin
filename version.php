a9556d8ff7b0465d668c4236542ce97d103773df
John McGettrick <jmcgettrick@iparadigms.com>
2015-06-10 15:44:23 +0100

    Increment version.

--- version.php
+++ version.php
@@ -5,9 +5,9 @@
  * and open the template in the editor.
  */
 
-$plugin->version =  2015040104;
+$plugin->version =  2015040105;
 $plugin->requires =  2012062500.00;
 $plugin->cron     = 300;
 $plugin->component = 'plagiarism_turnitin';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->dependencies = array('mod_turnitintooltwo' => 2015040104);
+$plugin->dependencies = array('mod_turnitintooltwo' => 2015040105);

d9a7d0be2f57dec72cea37b4770a5e6efc25c582
John McGettrick <jmcgettrick@turnitin.com>
2017-12-19 15:45:55 +0000

    Re-organise rubrics based on groups.

--- classes/turnitin_class.class.php
+++ classes/turnitin_class.class.php
@@ -61,11 +61,10 @@ class turnitin_class {
             $readclass = $response->getClass();
 
             $rubrics = $readclass->getSharedRubrics();
-            $rubricarray = array();
-            foreach ($rubrics as $rubric) {
-                $rubricarray[$rubric->getRubricId()] = $rubric->getRubricName()."
-                [".get_string('sharedrubric', 'plagiarism_turnitin')."]";
-            }
+			$rubricarray = array();
+			foreach ($rubrics as $rubric) {
+				$rubricarray[$rubric->getRubricGroupName()][$rubric->getRubricId()] = $rubric->getRubricName();
+			}
 
             $this->sharedrubrics = $rubricarray;
 

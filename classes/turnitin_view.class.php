3969379ea01accd281df9936488906dfc55fe4e3
David Winn <dwinn@turnitin.com>
2019-03-07 12:30:27 +0000

    Don't run the cron tasks if the plug can't connect to Turnitin.

--- classes/turnitin_view.class.php
+++ classes/turnitin_view.class.php
@@ -114,8 +114,8 @@ class turnitin_view {
 
             // This will ensure all rubric keys are integers.
             $rubricsnew = array(0 => get_string('norubric', 'plagiarism_turnitin'));
-            foreach ($rubrics AS $options => $rubriclist) {
-                foreach ($rubriclist AS $key => $value) {
+            foreach ($rubrics as $options => $rubriclist) {
+                foreach ($rubriclist as $key => $value) {
                     $rubricsnew[$key] = $value;
                 }
             }

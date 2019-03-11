e8c56ea828b3f8d1d102d5694ea8f3a4e90164fd
David Winn <dwinn@turnitin.com>
2019-03-11 16:45:39 +0000

    Disable Grademark if a grade does not exist.

--- lib.php
+++ lib.php
@@ -945,8 +945,16 @@ class plagiarism_plugin_turnitin extends plagiarism_plugin {
                         // Check if blind marking is on and revealidentities is not set yet.
                         $blindon = (!empty($moduledata->blindmarking) && empty($moduledata->revealidentities));
 
+                        // Check if a grade exists - as $currentgradequery->grade defaults to -1.
+                        $gradeExists = false;
+                        if (isset($currentgradequery->grade)) {
+                            if ($currentgradequery->grade >= 0) {
+                                $gradeExists = true;
+                            }
+                        }
+
                         // Can grade and feedback be released to this student yet?
-                        $released = ((!$blindon) && ($gradesreleased && (!empty($plagiarismfile->gm_feedback) || isset($currentgradequery->grade))));
+                        $released = ((!$blindon) && ($gradesreleased && (!empty($plagiarismfile->gm_feedback) || $gradeExists)));
 
                         // Show link to open grademark.
                         if ($config->usegrademark && ($istutor || ($linkarray["userid"] == $USER->id && $released))

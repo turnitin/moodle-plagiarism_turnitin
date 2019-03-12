8ea886aa46c06038477e96217bd630c72bc84d5c
David Winn <dwinn@turnitin.com>
2019-03-12 14:40:55 +0000

    Reset error code 13s on plugin upgrade.

--- version.php
+++ version.php
@@ -19,7 +19,7 @@
  * @copyright 2012 iParadigms LLC
  */
 
-$plugin->version = 2019011701;
+$plugin->version = 2019031202;
 $plugin->release = "2.7+";
 $plugin->requires = 2014051200;
 $plugin->component = 'plagiarism_turnitin';

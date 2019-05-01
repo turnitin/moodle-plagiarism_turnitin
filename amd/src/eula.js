e7ac3b296589b414fc79900a6adbfe9bf46346dd
David Winn <dwinn@turnitin.com>
2019-05-01 12:20:33 +0100

    EULA styles

--- amd/src/eula.js
+++ amd/src/eula.js
@@ -30,6 +30,8 @@ define(['jquery',
                     })
                         .then(function (modal) {
                             modal.show();
+                            modal.getRoot().find('.modal').addClass('tii_pp_modal_eula');
+                            modal.getRoot().find('.modal-content').addClass('tii_pp_modal_eula_content');
                         });
                 });
 

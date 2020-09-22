f7011b1af431979c9742eda51d56fdc99d132b37
cspinks <cspinks@turnitin.com>
2020-09-22 15:11:05 +0100

    Refactor if to pass setting

--- classes/turnitin_view.class.php
+++ classes/turnitin_view.class.php
@@ -276,7 +276,8 @@ class turnitin_view {
             $this->lock($mform, $location, $locks);
             $mform->addHelpButton('plagiarism_compare_journals', 'journalcheck', 'plagiarism_turnitin');
 
-            if ($config->plagiarism_turnitin_repositoryoption == 1) {
+            if ($config->plagiarism_turnitin_repositoryoption == PLAGIARISM_TURNITIN_ADMIN_REPOSITORY_OPTION_EXPANDED ||
+                $config->plagiarism_turnitin_repositoryoption == PLAGIARISM_TURNITIN_ADMIN_REPOSITORY_OPTION_FORCE_INSTITUTIONAL) {
                 $mform->addElement('select', 'plagiarism_compare_institution',
                                                 get_string('compareinstitution', 'plagiarism_turnitin'), $options);
                 $this->lock($mform, $location, $locks);

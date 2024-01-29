db3ad86427a526cc19dae3ba3dd3bf10a7b35644
Isaac Xiong <ixiong@turnitin.com>
2024-01-29 15:21:27 -0600

    fix modal dialogues

---
+++ amd/src/new_rubric.js
@@ -0,0 +1,59 @@
+/**
+ * Javascript controller for handling rubrics.
+ *
+ * @copyright Turnitin
+ * @author 2024 Isaac Xiong <ixiong@turnitin.com>
+ * @module plagiarism_turnitin/newRubric
+ */
+
+define(['jquery',
+        'core/templates',
+        'core/modal',
+        'core/modal_events',
+        'plagiarism_turnitin/modal_rubric_manager_launch',
+        'plagiarism_turnitin/modal_rubric_view_launch'
+    ],
+    function($, Templates, Modal, ModalEvents, ModalRubricManagerLaunch, ModalRubricViewLaunch) {
+        return {
+            newRubric: function() {
+                var that = this;
+                $('.rubric_manager_launch').on('click', function() {
+                    var courseid = $(this).data('courseid');
+                    var cmid = $(this).data('cmid');
+                    that.rubricCreateModal(ModalRubricManagerLaunch, courseid, cmid);
+                });
+
+                $(document).on('click', '.rubric_view', function() {
+                    var courseid = $(this).data('courseid');
+                    var cmid = $(this).data('cmid');
+                    that.rubricCreateModal(ModalRubricViewLaunch, courseid, cmid);
+                });
+
+                // Show warning when changing the rubric linked to an assignment.
+                $('#id_plagiarism_rubric').mousedown(function () {
+                    if ($('input[name="instance"]').val() != '' && $('input[name="rubric_warning_seen"]').val() != 'Y') {
+                        if (confirm(M.str.plagiarism_turnitin.changerubricwarning)) {
+                            $('input[name="rubric_warning_seen"]').val('Y');
+                        }
+                    }
+                });
+            },
+            rubricCreateModal: function(modalType, courseid, cmid) {
+                Modal.create({
+                    type: modalType.TYPE,
+                    template: modalType.TEMPLATE,
+                    templateContext: {
+                        courseid: courseid,
+                        cmid: cmid,
+                        wwwroot: M.cfg.wwwroot
+                    },
+                    large: true
+                })
+                    .then(function (modal) {
+                        modal.show();
+                        modal.getRoot().find('.modal').addClass('tii_pp_modal_rubric');
+                        modal.getRoot().find('.modal-content').addClass('tii_pp_modal_rubric_content');
+                    });
+            }
+        };
+    });

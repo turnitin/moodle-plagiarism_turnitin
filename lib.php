40e7beef13d403bdbb1cad293d062b1c749c987d
Jack <jmilgate@turnitin.com>
2024-09-06 12:20:45 +0100

    Indentation

--- lib.php
+++ lib.php
@@ -2045,54 +2045,49 @@ class plagiarism_plugin_turnitin extends plagiarism_plugin {
 
         // Add submission ids to the request.
         foreach ($submissions as $tiisubmission) {
+            // Updates the db field 'duedate_report_refresh' if the due date has passed within the last twenty four hours.
+            $moduledata = $DB->get_record($tiisubmission->modname, array('id' => $tiisubmission->instance));
+            $now = strtotime('now');
+            $dtdue = (!empty($moduledata->duedate)) ? $moduledata->duedate : 0;
+            if ($tiisubmission->duedate_report_refresh != 1 && $now >= $dtdue && $now < strtotime('+1 day', $dtdue)) {
+                $this->set_duedate_report_refresh($tiisubmission->id, 1);
+            }
 
-            // Only add the submission to the request if the module still exists.
-            // if ($cm = get_coursemodule_from_id('', $tiisubmission->cm)) {
+            if (!isset($reportsexpected[$tiisubmission->cm])) {
+                $plagiarismsettings = $this->get_settings($tiisubmission->cm);
 
-                // Updates the db field 'duedate_report_refresh' if the due date has passed within the last twenty four hours.
-                $moduledata = $DB->get_record($tiisubmission->modname, array('id' => $tiisubmission->instance));
-                $now = strtotime('now');
-                $dtdue = (!empty($moduledata->duedate)) ? $moduledata->duedate : 0;
-                if ($tiisubmission->duedate_report_refresh != 1 && $now >= $dtdue && $now < strtotime('+1 day', $dtdue)) {
-                    $this->set_duedate_report_refresh($tiisubmission->id, 1);
+                $reportsexpected[$tiisubmission->cm] = 1;
+
+                if (!isset($plagiarismsettings['plagiarism_compare_institution'])) {
+                    $plagiarismsettings['plagiarism_compare_institution'] = 0;
                 }
 
-                if (!isset($reportsexpected[$tiisubmission->cm])) {
-                    $plagiarismsettings = $this->get_settings($tiisubmission->cm);
-
-                    $reportsexpected[$tiisubmission->cm] = 1;
-
-                    if (!isset($plagiarismsettings['plagiarism_compare_institution'])) {
-                        $plagiarismsettings['plagiarism_compare_institution'] = 0;
-                    }
-
-                    // Don't add the submission to the request if module settings mean we will not get a report back.
-                    if (array_key_exists('plagiarism_compare_student_papers', $plagiarismsettings) &&
-                        $plagiarismsettings['plagiarism_compare_student_papers'] == 0 &&
-                        $plagiarismsettings['plagiarism_compare_internet'] == 0 &&
-                        $plagiarismsettings['plagiarism_compare_journals'] == 0 &&
-                        $plagiarismsettings['plagiarism_compare_institution'] == 0) {
-                        $reportsexpected[$tiisubmission->cm] = 0;
-                    }
+                // Don't add the submission to the request if module settings mean we will not get a report back.
+                if (array_key_exists('plagiarism_compare_student_papers', $plagiarismsettings) &&
+                    $plagiarismsettings['plagiarism_compare_student_papers'] == 0 &&
+                    $plagiarismsettings['plagiarism_compare_internet'] == 0 &&
+                    $plagiarismsettings['plagiarism_compare_journals'] == 0 &&
+                    $plagiarismsettings['plagiarism_compare_institution'] == 0) {
+                    $reportsexpected[$tiisubmission->cm] = 0;
                 }
+            }
 
-                // Only add the submission to the request if we are expecting an originality report.
-                if ($reportsexpected[$tiisubmission->cm] == 1) {
-                    $submissionids[] = $tiisubmission->externalid;
+            // Only add the submission to the request if we are expecting an originality report.
+            if ($reportsexpected[$tiisubmission->cm] == 1) {
+                $submissionids[] = $tiisubmission->externalid;
 
-                    // If submission is added to the request, add the corresponding assign id in the assignids array.
-                    $moduleturnitinconfig = $DB->get_record('plagiarism_turnitin_config',
-                        array(
-                            'cm' => $tiisubmission->cm,
-                            'name' => 'turnitin_assignid'
-                        )
-                    );
+                // If submission is added to the request, add the corresponding assign id in the assignids array.
+                $moduleturnitinconfig = $DB->get_record('plagiarism_turnitin_config',
+                    array(
+                        'cm' => $tiisubmission->cm,
+                        'name' => 'turnitin_assignid'
+                    )
+                );
 
-                    if (!isset(array_flip($assignmentids)[$moduleturnitinconfig->value])) {
-                        $assignmentids[] = $moduleturnitinconfig->value;
-                    }
+                if (!isset(array_flip($assignmentids)[$moduleturnitinconfig->value])) {
+                    $assignmentids[] = $moduleturnitinconfig->value;
                 }
-            // }
+            }
         }
 
         $validatedsubmissions = $this->check_local_submission_state($assignmentids, $submissionids);

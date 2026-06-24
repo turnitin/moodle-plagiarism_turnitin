b64f6e866edc7412f038b57cbdca14c3ff04da30
Jack <jmilgate@turnitin.com>
2026-06-24 14:13:06 +0100

    Null check for user->id

--- lib.php
+++ lib.php
@@ -1362,7 +1362,7 @@ class plagiarism_plugin_turnitin extends plagiarism_plugin {
         if ($cm->modname === 'assign' && !$istutor && !empty($plagiarismfile)) {
             $context = context_course::instance($cm->course);
             $assign = new assign($context, $cm, null);
-            if ($assign->get_instance()->teamsubmission && $plagiarismfile->submitter != $USER->id) {
+            if ($assign->get_instance()->teamsubmission && isset($USER->id) && $plagiarismfile->submitter != $USER->id) {
                 $output .= html_writer::tag('div', get_string('nonsubmittingstudentinfo', 'plagiarism_turnitin'), ['class' => 'tii_nonsubmitter_info']);
             }
         }

776a3d9cc9b5e9a4e013a7a6f79a191b0b6d1171
Jack <jmilgate@turnitin.com>
2026-06-30 13:54:00 +0100

    Use moodle checkforexisting instead of setting record as in flight

--- classes/task/send_submissions.php
+++ classes/task/send_submissions.php
@@ -74,16 +74,11 @@ class send_submissions extends \core\task\scheduled_task {
                 return;
             }
 
-            // Set the submissions status to in flight so subsequent cron runs don't try to queue them again.
-            // The status will be updated to either complete or error by the ad hoc task.
-            $DB->set_field_select("plagiarism_turnitin_files", "statuscode", "in_flight", 
-                "statuscode = 'queued' OR statuscode = 'pending'");
-
             mtrace('Found ' . count($queueditems) . ' queued submissions.');
             mtrace('Queueing ad-hoc tasks...');
             foreach ($queueditems as $item) {
               $adhoctask = adhoc_send_submission::instance($item);
-              \core\task\manager::queue_adhoc_task($adhoctask);
+              \core\task\manager::queue_adhoc_task($adhoctask, true);
               mtrace('  Queued submission for upload: ' . json_encode($item));
             }
             mtrace('Done.');
